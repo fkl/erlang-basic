367274b694dada5327e9c173226af08fece930e1
fkl <fayyazkl@gmail.com>
2019-07-03 18:44:50 -0700

    Trying to create a map from the list - not working
    {51, {{"name","bdQuotManager"}, {"tasks", [{"one", 12}, {"Two", 13}]}}}.
    {52, {{"name","bdMailManager"}, {"tasks", [{"one", 15}, {"Two", 22}]}}}.

--- rf.erl
+++ rf.erl
@@ -6,7 +6,14 @@ rd() ->
    %Txt = file:read_file("map_plain.txt"),
 
    Txt = file:consult("map_eb.txt"),
-   io:fwrite("~p~n",[Txt]),
+   List = [Txt],
+   io:fwrite("LIST FROM FILE ~p~n",List),
 
-   SMap = #{1=>{{"name","bdQuotaMgr"},{"taskIDs",[]}}, 2=>"second"},
-   io:format("~p~n", [maps:find(1,SMap)]).
+   %SMap = #{1=>{{"name","bdQuotaMgr"},{"taskIDs",[]}}, 2=>"second"},
+   L = [{ 51, {  {"name", "bdQuotManager"},{"tasks",[ {"one",12}, {"two", 22}  ]} } }, {52, {"name", "bdMailManager"}} ],
+   SMap = maps:from_list(L),
+
+
+   %io:format("LOCAL LIST ~p~n", L),
+   io:format("SMAP is ~p~n", [maps:find(51,SMap)]).
+~                                                          
