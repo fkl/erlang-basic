cd878d0398b67d32b4d8be218d18fdbed90a68d2
fkl <fayyazkl@gmail.com>
2019-07-03 19:09:11 -0700

    working version with below map_eb.txt file
    {51, {{"name","bdQuotManager"}, {"tasks", [{"one", 12}, {"Two", 13}]}}}.
    {52, {{"name","bdMailManager"}, {"tasks", [{"one", 15}, {"Two", 22}]}}}.

--- rf.erl
+++ rf.erl
@@ -5,15 +5,17 @@ rd() ->
    %{ok, File} = file:open("map.txt",[read]),
    %Txt = file:read_file("map_plain.txt"),
 
-   Txt = file:consult("map_eb.txt"),
-   List = [Txt],
-   io:fwrite("LIST FROM FILE ~p~n",List),
+   {ok, Txt} = file:consult("map_eb.txt"),
+   List = Txt,
+
+   io:fwrite("LIST FROM FILE ~p~n",[List]),
 
    %SMap = #{1=>{{"name","bdQuotaMgr"},{"taskIDs",[]}}, 2=>"second"},
-   L = [{ 51, {  {"name", "bdQuotManager"},{"tasks",[ {"one",12}, {"two", 22}  ]} } }, {52, {"name", "bdMailManager"}} ],
-   SMap = maps:from_list(L),
+   %L = [{ 51, {  {"name", "bdQuotManager"},{"tasks",[ {"one",12}, {"two", 22}  ]} } }, {52, {"name", "bdMailManager"}} ],
+
+   SMap = maps:from_list(List),
 
 
-   %io:format("LOCAL LIST ~p~n", L),
+   %io:fwrite("LOCAL LIST ~p~n", [L]),
    io:format("SMAP is ~p~n", [maps:find(51,SMap)]).
-~                                                          
+~                                                      
