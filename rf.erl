674388a5eca5e8d1aaae5e155478f8e95d06b008
fkl <fayyazkl@gmail.com>
2019-07-03 19:14:46 -0700

    just clean up
    Removed un used code for now. Next, create nested level map

--- rf.erl
+++ rf.erl
@@ -2,20 +2,10 @@
 -export([rd/0]).
 
 rd() ->
-   %{ok, File} = file:open("map.txt",[read]),
-   %Txt = file:read_file("map_plain.txt"),
-
    {ok, Txt} = file:consult("map_eb.txt"),
    List = Txt,
-
    io:fwrite("LIST FROM FILE ~p~n",[List]),
-
-   %SMap = #{1=>{{"name","bdQuotaMgr"},{"taskIDs",[]}}, 2=>"second"},
-   %L = [{ 51, {  {"name", "bdQuotManager"},{"tasks",[ {"one",12}, {"two", 22}  ]} } }, {52, {"name", "bdMailManager"}} ],
 
    SMap = maps:from_list(List),
 
-
-   %io:fwrite("LOCAL LIST ~p~n", [L]),
    io:format("SMAP is ~p~n", [maps:find(51,SMap)]).
-~                                                      
