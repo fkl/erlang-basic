b240735f7134523a4c1aaf6ca72480c993ff3512
fkl <fayyazkl@gmail.com>
2019-07-23 15:32:49 -0700

    given json convert to list and iterate each
    Given a nested json, convert to list and iterate each entry
    Following is the ouput
    
    1> json:jmap().
    JSON IS {obj,[{"services",
                   [{obj,[{"tasks",[]},{"id",51},{"name","bdQuotaManager"}]},
                    {obj,[{"tasks",[{{"id",1},{"name","getOwnerMessages"}}]}, 
                          {"id",104},
                          {"name","bdCommService"}]}]}]}
    ELEMENT IS  obj
    ELEMENT IS  [{"services",
                  [{obj,[{"tasks",[]},{"id",51},{"name","bdQuotaManager"}]},
                   {obj,[{"tasks",[{{"id",1},{"name","getOwnerMessages"}}]},
                         {"id",104},
                         {"name","bdCommService"}]}]}]
    ok

---
+++ json.erl
@@ -0,0 +1,22 @@
+-module(json).
+-export([jmap/0]).
+
+jmap() ->
+
+   Json = {obj,[{"services",
+       [{obj,[{"tasks",[]},{"id",51},{"name","bdQuotaManager"}]},
+	{obj,[{"tasks",[{{"id",1}, {"name", "getOwnerMessages"}}]}, {"id",104}, {"name","bdCommService"}]}
+              ]}]},
+
+   io:format("JSON IS ~p~n", [Json]),
+
+   List = tuple_to_list(Json),
+
+   Func = fun(Elem) ->
+	if Elem == "obj" -> io:format("Nothing");
+		true -> io:format("ELEMENT IS  ~p~n", [Elem])
+	end
+   end,
+
+   lists:foreach(Func, List).
+   
