f6f2b00a4ea0cb4823406a111eb7e4bd4b24f352
fkl <fayyazkl@gmail.com>
2019-07-24 19:13:21 -0700

    Added record and get key list but not working

--- json.erl
+++ json.erl
@@ -1,6 +1,20 @@
 -module(json).
 -export([jmap/0]).
 
+-record(taskrec,
+        {
+            id  :: integer(),
+            name :: string()}).
+
+-record(services,
+        {tasks            :: [taskrec],
+         id                 :: integer(),
+         name               :: string()}).
+
+get_keylist_val(Key, KeyList) ->
+    {Key, Val} = lists:keyfind(Key, 1, KeyList),
+    Val.
+
 jmap() ->
 
    Json = {obj,[{"services",
@@ -8,15 +22,28 @@ jmap() ->
 	{obj,[{"tasks",[{{"id",1}, {"name", "getOwnerMessages"}}]}, {"id",104}, {"name","bdCommService"}]}
               ]}]},
 
-   io:format("JSON IS ~p~n", [Json]),
-
-   List = tuple_to_list(Json),
-
-   Func = fun(Elem) ->
-	if Elem == obj -> io:format("Nothing");
-		true -> io:format("ELEMENT IS  ~p~n", [Elem])
-	end
+   case get_keylist_val("services", Json) of
+      <<"tasks">> ->
+         Result = {ok, #services{
+            tasks =  [get_keylist_val("tasks", Json)],
+            id =  get_keylist_val("id", Json),
+            name = get_keylist_val("name", Json)}},
+            io:format("Result is ~p~n", Result);
+      _ -> {error, bad_format}
    end,
 
-   lists:foreach(Func, List).
+   io:format("JSON IS ~p~n", [ Json ]).
+
+   %List = tuple_to_list(Json),
+
+   %Func = fun(Elem) ->
+	%if Elem =/= obj -> io:format("ELEMENT IS  ~p~n", [Elem]);
+	%	true -> io:format("~n") 
+	%end
+   %end,
    
+   %{services, [ServiceList] } = List,
+   %lists:foreach(Func, List).
+   
+   %SMap = maps:from_list(List),
+   %io:format("after conversion ~p~n", List[0]).
